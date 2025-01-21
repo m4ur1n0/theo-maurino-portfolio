fc51cca1d0b3768aa104043c8c460a7cd80af2a4
theo <thjmaurino@gmail.com>
2025-01-21 12:27:58 -0600

    Added download button

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,24 @@
+// import {resume} from '../files/resume.pdf'
 export default function NavBar({selected, setSelected}) {
+
+  const onButtonClick = () => {
+    
+    // using Java Script method to get PDF file
+    fetch(".. /files/resume.pdf").then((response) => {
+        response.blob().then((blob) => {
+        
+            // Creating new object of PDF file
+            const fileURL =
+                window.URL.createObjectURL(blob);
+                
+            // Setting various property values
+            let alink = document.createElement("a");
+            alink.href = fileURL;
+            alink.download = "theo_maurino_resume.pdf";
+            alink.click();
+        });
+    });
+  };
 
   const options = [
     {
@@ -51,17 +71,26 @@ export default function NavBar({selected, setSelected}) {
 
         </div>
 
-        <div className="socials-links flex justify-between items-center mt-36 w-2/3">
+        <div className="socials-links flex justify-between items-center mt-36 w-3/4">
           <a className="" href='https://www.linkedin.com/in/theo-maurino-829929239/' target='_blank'>
-            <img className="w-1/3 color-text_color" src='/vectors/linkedin.svg' />
+            <img className="w-1/4 color-text_color" src='/vectors/linkedin.svg' />
           </a>
 
           <a className="" href='https://www.github.com/m4ur1n0/' target='_blank'>
-            <img className="w-1/3" src='/vectors/github.svg' />
+            <img className="w-1/4" src='/vectors/github.svg' />
           </a>
 
           <a className="" href='https://mailto:thjmaurino@gmail.com' target='_blank'>
             <img className="scale-[3.2]" src='/vectors/gmail.svg' />
+          </a>
+
+          {/* <a className="" href={resume} download={'theo_maurino_resume'} target='_blank'>
+            <img className="w-1/3" src='/download/gmail.svg' />
+          </a> */}
+
+
+          <a href={"/files/resume.pdf"} download="theo_maurino">
+            <img className="w-1/4 mx-12 " src='/vectors/download.svg' />
           </a>
         </div>
         
